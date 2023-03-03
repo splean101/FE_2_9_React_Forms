c94a748817ad1ed99ff6c6bdffdc2cb3be67ee6a
splean101 <101splean@gmail.com>
2023-03-03 08:09:15 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function App () {
+
+return (
+<div className="wrapper">
+  <form>
+    <label>Name</label>
+    <input />
+    <label>Login</label>
+    <input />
+    <label>Age</label>
+    <input type="number" />
+    <label>Email</label>
+    <input />
+    <button type="submit">Sign in</button>
+  </form>
+</div>
+);
+} 
+
+export default App;
